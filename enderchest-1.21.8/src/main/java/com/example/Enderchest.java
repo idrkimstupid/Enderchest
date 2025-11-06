d21f2d94fab0cbb95510e64ad6b921c8c9a33370
idrkimstupid <wokwokwok817@gmail.com>
2025-11-06 17:20:26 +0100

    Add files via upload

---
+++ enderchest-1.21.8/src/main/java/com/example/Enderchest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.util.ActionResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+public class Enderchest implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
+
